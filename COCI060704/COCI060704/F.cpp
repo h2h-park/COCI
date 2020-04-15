edee0e077eef41284d4b2953923b64480566df64
Heehyun Park <heehyunbpark@gmail.com>
2020-04-15 19:43:17 -0400

    add F

--- COCI060704/COCI060704/F.cpp
+++ COCI060704/COCI060704/F.cpp
@@ -3,72 +3,82 @@
 #define MAX 200010
 
 struct data {
-	int A, B, i;
+    int A, B, i;
 };
 int n, m;
 data stu[MAX];
 
-int tree[MAX];
-int input[MAX][3];
+int term = 1;
+int tree[1 << 19];
+int input[MAX][3], ord[MAX];
 
 void update(int k, int x) {
-	tree[x] = k;
-	for (; x != 1; x >>= 1) {
-		if (tree[x] <= tree[x >> 1]) break;
-		tree[x >> 1] = tree[x];
-	}
+    tree[x] = k;
+    for (; x != 1; x >>= 1) {
+        if (tree[x] <= tree[x >> 1]) break;
+        tree[x >> 1] = tree[x];
+    }
+}
+
+int find(int x, int to, int L, int R, int v) {
+    if (to >= R || L > term + n || tree[x] < v) {
+        return -1;
+    }
+
+    if (L == R) {
+        return L - term;
+    }
+    int mid = (L + R) / 2;
+    int t;
+    t = find(x * 2, to, L, mid, v);
+    if (t != -1) return t;
+    return find(x * 2 + 1, to, mid + 1, R, v);
 }
 
 int main(void) {
-	int i, j;
-	char c;
-	scanf("%d", &m);
-	for (i = 1; i <= m; i++) {
-		scanf(" %c", &c);
-		if (c == 'D') {
-			scanf("%d %d", &stu[++n].A, &stu[n].B);
-			input[i][1] = stu[n].A, input[i][2] = stu[n].B;
-			stu[n].i = n;
-		} else {
-			scanf("%d", &j);
-			input[i][1] = j;
-		}
-	}
-	std::sort(stu + 1, stu + n + 1, [](const data& a, const data& b) {
-		return (a.B != b.B) ? a.B < b.B : a.A < b.A;
-		});
-	for (i = 1; i <= n; i++) stu[i].B = i;
-	std::sort(stu + 1, stu + n + 1, [](const data& a, const data& b) {
-		return a.i < b.i;
-		});
+    int i, j;
+    char c;
+    scanf("%d", &m);
+    for (i = 1; i <= m; i++) {
+        scanf(" %c", &c);
+        if (c == 'D') {
+            ++n;
+            scanf("%d %d", &stu[n].A, &stu[n].B);
+            input[i][1] = stu[n].A, input[i][2] = stu[n].B;
+            stu[n].i = n;
+        }
+        else {
+            scanf("%d", &j);
+            input[i][1] = j;
+            input[i][0] = 1;
+        }
+    }
+    std::sort(stu + 1, stu + n + 1, [](const data& a, const data& b) {
+        return (a.B != b.B) ? a.B < b.B : a.A < b.A;
+        });
+    for (i = 1; i <= n; i++) {
+        stu[i].B = i;
+    }
+    std::sort(stu + 1, stu + n + 1, [](const data& a, const data& b) {
+        return a.i < b.i;
+        });
 
-	int term = 1, dep = 0;
-	for (; term < n; term <<= 1, dep++);
-	term--;
-	for (i = 1; i <= m; i++) {
-		if (input[i][0] == 0) {
-			update(stu[i].B, term + stu[i].A);
-		}
-		else {
-			int x = 1, X, Y;
-			int p = stu[i].B;
-			for (i = 0; i < dep; i++) {
-				X = x * (1 << (dep - i));
-				Y = x * (1 << (dep - i)) + (1 << (dep - i - 1)) - 1;
-				if (X <= x && x <= Y) {
+    for (i = 1; i <= n; i++) {
+        ord[stu[i].B] = i;
+    }
 
-				}
-				X = Y + 1;
-				Y = (x + 1) * (1 << (dep - i)) - 1;
-				if (X <= x && x <= Y) {
-					x = x * 2 + 1;
-					continue;
-				}
-
-				puts("NE");
-				break;
-			}
-		}
-	}
-	return false;
+    for (; term < n; term <<= 1);
+    term--;
+    for (i = 1, j = 0; i <= m; i++) {
+        if (input[i][0] == 0) {
+            j++;
+            update(stu[j].A, term + stu[j].B);
+        }
+        else {
+            int t = find(1, term + stu[input[i][1]].B, term + 1, term * 2 + 1, stu[input[i][1]].A);
+            if (t == -1) puts("NE");
+            else printf("%d\n", ord[t]);
+        }
+    }
+    return false;
 }
