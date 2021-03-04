06defead42643b6e89d7e518c04b163284b9f98e
h2h-park <jsa3824@gmail.com>
2021-03-04 03:06:50 -0500

    A

--- COCI202105/A.cpp
+++ COCI202105/A.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include <string>
+#include <set>
 
 using namespace std;
-string st;
+string st, subst;
+set<string> S;
 
-int main(void) {
+int main(void){
+    int sz = 0;
     cin >> st;
-    
+    sz = st.size();
+    for (int i = 0; i < sz; i++) {
+        if (st[i] >= '0' && st[i] <='9') {
+            subst = st[i];
+            for (++i; i < sz; i++) {
+                if (!(st[i] >= '0' && st[i] <='9')) break;
+                subst += st[i];
+            }
+            S.insert(subst);
+        }
+    }
+    cout << S.size() << endl;
     return false;
 }
